4d148fd93d8ed4caa220f239461da45bff8ca5ec
KHADIJAIMOU <imkhadija446@gmail.com>
2023-04-13 13:06:40 +0000

    edit

--- database/migrations/layouts/footer.blade.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-    <strong>Copyright &copy;Tous droits réservés | Ce site web est réalisé par l'équipe BodyB.</strong>
-    <div class="float-right d-none d-sm-inline-block">
-      {{-- <b>Version</b> 3.2.0 --}}
-    </div>
-  </footer>
